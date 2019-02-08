89fa8fa0ff612ce2fc669eda7fa276cbb81ef8fb
Stephen Lake <stephen@closurecode.com>
2019-02-08 20:08:56 +0200

    Update routes in accordance to config

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -8,14 +8,14 @@ Route::get(config('sanity.endpoints.forge.webhook', '/sanity/forge'), function (
     \Facades\Sanity\Factory::runRunners(request()->all());
 });
 
-Route::get(config('sanity.routes.unit', '/sanity/badges/unit.svg'), function () {
+Route::get(config('sanity.badges.unit', '/sanity/badges/unit.svg'), function () {
     return \Facades\Sanity\Badges::unit();
 });
 
-Route::get(config('sanity.routes.style', '/sanity/badges/style.svg'), function () {
+Route::get(config('sanity.badges.style', '/sanity/badges/style.svg'), function () {
     return \Facades\Sanity\Badges::style();
 });
 
-Route::get(config('sanity.routes.dusk', '/sanity/badges/dusk.svg'), function () {
+Route::get(config('sanity.badges.dusk', '/sanity/badges/dusk.svg'), function () {
     return \Facades\Sanity\Badges::dusk();
 });
