78f9f9697d4c3e254af5cb10e3b006b985e738a6
Stephen Lake <stephen@closurecode.com>
2019-02-09 04:47:41 +0200

    Remove test code

--- src/Subscriber.php
+++ src/Subscriber.php
@@ -16,8 +16,6 @@ class Subscriber
         $runnerClass = $e->runner;
         $runnerState = $runnerClass->passing() ? 'Success' : 'Failure';
         $runnerMethod = "on{$runnerClass->getName()}{$runnerState}";
-
-        echo 'hear event';
         
         if (method_exists($this, $runnerMethod)) {
             $this->$runnerMethod($runner);
